e37d20302ce78c5763f8afeda954941eec2c8ba4
Robbie Greiner <robbieg@gmail.com>
2017-10-23 15:49:39 -0600

    setup boilerplate stuff setup sass

--- src/configureStore.js
+++ src/configureStore.js
@@ -1,7 +1,7 @@
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 // import rootReducer from './index.js';
-import createBrowserHistory from '../../node_modules/history/createBrowserHistory';
+import createBrowserHistory from 'history/createBrowserHistory';
 import { routerMiddleware } from 'react-router-redux';
 // import DevTools from '../containers/DevTools.js';
 
--- src/index.js
+++ src/index.js
@@ -1,8 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import './styles/index.css';
+import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './configureStore.js';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={store}>
+    <BrowserRouter>
+      <div>
+        <App />
+      </div>
+    </BrowserRouter>
+  </Provider>,
+  document.getElementById('root')
+);
 registerServiceWorker();
