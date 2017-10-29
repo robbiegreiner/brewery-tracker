bb836b19e459a6244303a543c6ca246882ce0138
Robbie Greiner <robbieg@gmail.com>
2017-10-29 10:56:56 -0600

    changed brewery from functional to react component

--- src/components/Brewery.js
+++ src/components/Brewery.js
@@ -1,25 +1,41 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { Redirect } from 'react-router-dom';
 
-const Brewery = ({ brewery }) => {
-  if (brewery.images) {
+class Brewery extends Component {
+  constructor() {
+    super();
+  }
+
+  renderBrewery() {
+    const { brewery } = this.props;
+    if (brewery.images) {
+      return (
+        <div className='brewery'>
+          <h3>Brewery</h3>
+          <h2>{brewery.name}</h2>
+          <img className='brewery-logo' src={brewery.images.large}></img>
+          <h4>{brewery.website}</h4>
+          <p>{brewery.description ? brewery.description : 'no description provided'}</p>
+        </div>
+      );
+    } else {
+      return (
+        <h4>Brewery</h4>
+      );
+    }
+  }
+
+  render() {
     return (
-      // link to brewery page on click
-      <div className='brewery'>
-        <h3>Brewery</h3>
-        <h2>{brewery.name}</h2>
-        <img className='brewery-logo' src={brewery.images.large}></img>
-        <h4>{brewery.website}</h4>
-        <p>{brewery.description ? brewery.description : 'no description provided'}</p>
+      <div>
+        {this.renderBrewery()}
       </div>
     );
-  } else {
-    return (
-      <h4>Brewery</h4>
-    );
   }
-};
+
+}
+
 
 Brewery.propTypes = {
   brewery: PropTypes.object
