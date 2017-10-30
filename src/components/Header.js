19c5982da46bc35fdb4e012e9172cf414521d563
Robbie Greiner <robbieg@gmail.com>
2017-10-30 17:29:06 -0600

    added drop down

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,11 +5,39 @@ import pint from '../assets/pint.svg';
 
 
 const Header = () => {
+  function dropDown() {
+    document.getElementById("myDropdown").classList.toggle("show");
+  } 
+
+  window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+
+      var dropdowns = document.getElementsByClassName("dropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  };
+
+
+
   return (
     <div className='header'>
       <Link to='/'>
         <h1>Brewery <img className='header-logo' src={pint}></img> Tracker</h1>
       </Link>
+      <div className="dropdown">
+        <button onClick={dropDown} className="dropbtn">Dropdown</button>
+        <div id="myDropdown" className="dropdown-content">
+          <a href="#">Link 1</a>
+          <a href="#">Link 2</a>
+          <a href="#">Link 3</a>
+        </div>
+      </div>
     </div>
   );
 };
