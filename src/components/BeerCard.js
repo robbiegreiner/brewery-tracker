b7943d4d0a97acc6e0d9d4c60a69ab68e43091cd
Robbie Greiner <robbieg@gmail.com>
2017-11-03 09:25:45 -0600

    fixed links on cards

--- src/components/BeerCard.js
+++ src/components/BeerCard.js
@@ -4,15 +4,14 @@ import { Link } from 'react-router-dom';
 
 const BeerCard = ({ beer, setCurrentBeer, writeUserData, user }) => {
   return (
-    <Link to={'/beer/' + beer.id}>
-      <div className='beer-card' >
-        <h2 onClick={() => setCurrentBeer(beer)}>{beer.name}</h2>
-        <h3>{beer.brewies ? beer.breweries[0].name : 'no brewery info'}</h3>
-        <h3>{beer.style ? beer.style.name : 'no style'}</h3>
-        <h3>{beer.abv}</h3>
-        <h4 onClick={() => writeUserData(user.id, 'beer', beer.id )}>FAVORITE</h4>
-      </div>
-    </Link>
+    <div className='beer-card' >
+      <h2 onClick={() => setCurrentBeer(beer)}>{beer.name}</h2>
+      <h3>{beer.brewies ? beer.breweries[0].name : 'no brewery info'}</h3>
+      <h3>{beer.style ? beer.style.name : 'no style'}</h3>
+      <h3>{beer.abv}</h3>
+      <Link to={'/beer/' + beer.id}><h4>View Beer Details</h4></Link>
+      <h4 onClick={() => writeUserData(user.id, 'beer', beer.id )}>FAVORITE</h4>
+    </div>
   );
 };
 
--- src/components/BreweryCard.js
+++ src/components/BreweryCard.js
@@ -4,13 +4,13 @@ import { Link } from 'react-router-dom';
 
 const BreweryCard = ({ brewery, getBrewery, writeUserData, user }) => {
   return (
-    <Link to={'/brewery/' + brewery.id}>
-      <div className='beer-card'>
+    <div className='beer-card'>
+      <Link to={'/brewery/' + brewery.id}>
         <h2 onClick={() => getBrewery(brewery.id)}>{brewery.name}</h2>
-        <h3>{brewery.location ? brewery.locations[0].locality + ", " + brewery.locations[0].region : ''}</h3>
-        <h4 onClick={() => writeUserData(user.id, 'brewery', brewery.id)}>FAVORITE</h4>
-      </div>
-    </Link>
+      </Link>
+      <h3>{brewery.location ? brewery.locations[0].locality + ", " + brewery.locations[0].region : ''}</h3>
+      <h4 onClick={() => writeUserData(user.id, 'brewery', brewery.id)}>FAVORITE</h4>
+    </div>
   );
 };
 
