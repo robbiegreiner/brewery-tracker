e5738a7d2a3be56dac1221f8edc4e27c154b7376
Robbie Greiner <robbieg@gmail.com>
2017-11-05 11:22:04 -0700

    wired up link to feature beer

--- src/components/FeatureBeer.js
+++ src/components/FeatureBeer.js
@@ -2,20 +2,18 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 
-const FeatureBeer = ({ featureBeer, getBrewery, setCurrentBeer }) => {
-
-  const showBrewery = id => {
-    getBrewery(id);
-  };
+const FeatureBeer = ({ featureBeer, setCurrentBeer }) => {
 
 
   if (featureBeer.breweries) {
     return (
-      <div className='feature-beer' onClick={() => showBrewery(featureBeer.breweries[0].id)}>
+      <div className='feature-beer'>
         <h1>Featured Beer</h1>
-        <h2>{featureBeer.name}</h2>
-        <Link to='/brewery'><img className='brewery-logo-2' src={featureBeer.labels.medium}></img></Link>
-        <h3>{featureBeer.breweries[0].name}</h3>
+        <Link onClick={() => setCurrentBeer(featureBeer)} to={'/beer/' + featureBeer.id}>
+          <h2>{featureBeer.name}</h2>
+          <img className='brewery-logo-2' src={featureBeer.labels.medium}></img>
+          <h3>{featureBeer.breweries[0].name}</h3>
+        </Link>
         <a href={featureBeer.breweries[0].website}>{featureBeer.breweries[0].website}</a>
         <h3 className='feature-location'>{featureBeer.breweries[0].locations[0].locality}, {featureBeer.breweries[0].locations[0].region}</h3>
         <p>{featureBeer.description}</p>
