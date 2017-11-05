4fc0a36361ce51b001940bfff4bab8b337c95f2e
Robbie Greiner <robbieg@gmail.com>
2017-11-05 09:45:51 -0700

    redirect to login page if not logged in and try to fav

--- src/components/BeerCard.js
+++ src/components/BeerCard.js
@@ -4,20 +4,38 @@ import { Link } from 'react-router-dom';
 import beer1 from '../assets/beer2.svg';
 
 const BeerCard = ({ beer, setCurrentBeer, addFavoriteBeer, user, removeFavoriteBeer }) => {
-  return (
-    <div className={ beer.isFav ? 'favorite-card' : 'beer-card' }>
-      <h2 onClick={() => setCurrentBeer(beer)}>{beer.name}</h2>
-      <h3>{beer.breweries ? beer.breweries[0].name : ''}</h3>
-      <h4>{beer.breweries ? beer.breweries[0].locations[0].region: ''}</h4>
-      {beer.labels ? <img className='beer-logo' src={beer.labels.medium}></img> : <img className='beer-icon' src={beer1}></img> }
-      <h3 className='style'>{beer.style ? beer.style.name : ''}</h3>
-      <h3 className='abv'>ABV: {beer.abv}</h3>
-      <div className='bottom-btns'>
-        <Link to={'/beer/' + beer.id}><h4 className='view-beer'>DETAILS</h4></Link>
-        { removeFavoriteBeer ? <h4 className='favorite' onClick={() => removeFavoriteBeer(user.id, beer.firebaseID)}>UNFAVORITE</h4> : <h4 className='favorite' onClick={() => addFavoriteBeer(user.id, 'beer', beer.id, beer )}>FAVORITE</h4>}
+  if (user.email){
+    return (
+      <div className={ beer.isFav ? 'favorite-card' : 'beer-card' }>
+        <h2 onClick={() => setCurrentBeer(beer)}>{beer.name}</h2>
+        <h3>{beer.breweries ? beer.breweries[0].name : ''}</h3>
+        <h4>{beer.breweries ? beer.breweries[0].locations[0].region: ''}</h4>
+        {beer.labels ? <img className='beer-logo' src={beer.labels.medium}></img> : <img className='beer-icon' src={beer1}></img> }
+        <h3 className='style'>{beer.style ? beer.style.name : ''}</h3>
+        <h3 className='abv'>ABV: {beer.abv}</h3>
+        <div className='bottom-btns'>
+          <Link to={'/beer/' + beer.id}><h4 className='view-beer'>DETAILS</h4></Link>
+          { removeFavoriteBeer ? <h4 className='favorite' onClick={() => removeFavoriteBeer(user.id, beer.firebaseID)}>UNFAVORITE</h4> : <h4 className='favorite' onClick={() => addFavoriteBeer(user.id, 'beer', beer.id, beer )}>FAVORITE</h4>}
+        </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    return (
+      <div className={ beer.isFav ? 'favorite-card' : 'beer-card' }>
+        <h2 onClick={() => setCurrentBeer(beer)}>{beer.name}</h2>
+        <h3>{beer.breweries ? beer.breweries[0].name : ''}</h3>
+        <h4>{beer.breweries ? beer.breweries[0].locations[0].region: ''}</h4>
+        {beer.labels ? <img className='beer-logo' src={beer.labels.medium}></img> : <img className='beer-icon' src={beer1}></img> }
+        <h3 className='style'>{beer.style ? beer.style.name : ''}</h3>
+        <h3 className='abv'>ABV: {beer.abv}</h3>
+        <div className='bottom-btns'>
+          <Link to={'/beer/' + beer.id}><h4 className='view-beer'>DETAILS</h4></Link>
+          <Link to='/login'><h4 className='favorite'>FAVORITE</h4></Link>
+        </div>
+      </div>
+    );
+  }
+
 };
 
 BeerCard.propTypes = {
--- src/components/BreweryCard.js
+++ src/components/BreweryCard.js
@@ -4,18 +4,33 @@ import { Link } from 'react-router-dom';
 import beer1 from '../assets/tap.svg';
 
 const BreweryCard = ({ brewery, getBrewery, addFavoriteBrewery, user, removeFavoriteBrewery, favorites }) => {
-  return (
-    <div className={ brewery.isFav ? 'beer-card brewery-card favorite-card' : 'beer-card brewery-card' }>
-      <Link to={'/brewery/' + brewery.id}>
-        <h2 onClick={() => getBrewery(brewery.id)}>{brewery.name}</h2>
-      </Link>
-      <h3>{brewery.locations ? brewery.locations[0].locality + ", " + brewery.locations[0].region : ''}</h3>
-      {brewery.images ? <img className='brewery-logo' src={brewery.images.squareMedium}></img>: <img className='brewery-icon' src={beer1}></img>}
-      <a className='website' href={brewery.website}>{brewery.website}</a>
-      <h3 className='type'>{brewery.locations ? brewery.locations[0].locationTypeDisplay : ''}</h3>
-      { removeFavoriteBrewery ? <h4 className='favorite' onClick={() => removeFavoriteBrewery(user.id, brewery.firebaseID)}>UNFAVORITE</h4> : <h4 className='favorite' onClick={() => addFavoriteBrewery(favorites, user.id, 'brewery', brewery.id, brewery)}>FAVORITE</h4> }
-    </div>
-  );
+  if(user.email) {
+    return (
+      <div className={ brewery.isFav ? 'beer-card brewery-card favorite-card' : 'beer-card brewery-card' }>
+        <Link to={'/brewery/' + brewery.id}>
+          <h2 onClick={() => getBrewery(brewery.id)}>{brewery.name}</h2>
+        </Link>
+        <h3>{brewery.locations ? brewery.locations[0].locality + ", " + brewery.locations[0].region : ''}</h3>
+        {brewery.images ? <img className='brewery-logo' src={brewery.images.squareMedium}></img>: <img className='brewery-icon' src={beer1}></img>}
+        <a className='website' href={brewery.website}>{brewery.website}</a>
+        <h3 className='type'>{brewery.locations ? brewery.locations[0].locationTypeDisplay : ''}</h3>
+        { removeFavoriteBrewery ? <h4 className='favorite' onClick={() => removeFavoriteBrewery(user.id, brewery.firebaseID)}>UNFAVORITE</h4> : <h4 className='favorite' onClick={() => addFavoriteBrewery(favorites, user.id, 'brewery', brewery.id, brewery)}>FAVORITE</h4> }
+      </div>
+    );
+  } else {
+    return (
+      <div className={ brewery.isFav ? 'beer-card brewery-card favorite-card' : 'beer-card brewery-card' }>
+        <Link to={'/brewery/' + brewery.id}>
+          <h2 onClick={() => getBrewery(brewery.id)}>{brewery.name}</h2>
+        </Link>
+        <h3>{brewery.locations ? brewery.locations[0].locality + ", " + brewery.locations[0].region : ''}</h3>
+        {brewery.images ? <img className='brewery-logo' src={brewery.images.squareMedium}></img>: <img className='brewery-icon' src={beer1}></img>}
+        <a className='website' href={brewery.website}>{brewery.website}</a>
+        <h3 className='type'>{brewery.locations ? brewery.locations[0].locationTypeDisplay : ''}</h3>
+        <Link to='/login'><h4 className='favorite'>FAVORITE</h4></Link> 
+      </div>
+    );
+  }
 };
 
 BreweryCard.propTypes = {
