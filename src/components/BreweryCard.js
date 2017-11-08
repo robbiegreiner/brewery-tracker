d901942fcd8ac7c9c9217cf4463d799baef81056
Robbie Greiner <robbieg@gmail.com>
2017-11-08 10:00:53 -0700

    added string interpolation

--- src/components/BreweryCard.js
+++ src/components/BreweryCard.js
@@ -7,7 +7,7 @@ const showFavoriteButton = (user, brewery, removeFavoriteBrewery, getBrewery, ad
   if (user.email) {
     return (
       <div className='bottom-btns'>
-        <Link to={'/brewery/' + brewery.id}>
+        <Link to={`/brewery/${brewery.id}`}>
           <button className='view-beer' onClick={() => getBrewery(brewery.id)}>DETAILS</button>
         </Link>
         { removeFavoriteBrewery ? <button className='unfavorite favorite' onClick={() => removeFavoriteBrewery(user.id, brewery.firebaseID)}>UNFAVORITE</button> : <button className='favorite' onClick={() => addFavoriteBrewery(favorites, user.id, 'brewery', brewery.id, brewery)}>FAVORITE</button> }
@@ -15,7 +15,7 @@ const showFavoriteButton = (user, brewery, removeFavoriteBrewery, getBrewery, ad
     );
   } else {
     return <div className='bottom-btns'>
-      <Link to={'/brewery/' + brewery.id}>
+      <Link to={`/brewery/${brewery.id}`}>
         <button className='view-beer' onClick={() => getBrewery(brewery.id)}>DETAILS</button>
       </Link>
       <Link to='/login'><button className='favorite'>FAVORITE</button></Link>
@@ -27,11 +27,11 @@ const BreweryCard = ({ brewery, getBrewery, addFavoriteBrewery, user, removeFavo
   if (brewery) {
     return (
       <div className={ brewery.isFav ? 'beer-card brewery-card favorite-card' : 'beer-card brewery-card' }>
-        <Link to={'/brewery/' + brewery.id}>
+        <Link to={`/brewery/${brewery.id}`}>
           <h2 onClick={() => getBrewery(brewery.id)}>{brewery.name}</h2>
         </Link>
         <h3>{brewery.locations ? brewery.locations[0].locality + ", " + brewery.locations[0].region : ''}</h3>
-        <Link to={'/brewery/' + brewery.id}>
+        <Link to={`/brewery/${brewery.id}`}>
           {brewery.images ? <img onClick={() => getBrewery(brewery.id)} className='brewery-logo' src={brewery.images.squareMedium}></img>: <img onClick={() => getBrewery(brewery.id)} className='brewery-icon' src={beer1}></img>}
         </Link>
         <a className='website' href={brewery.website}>{brewery.website}</a>
