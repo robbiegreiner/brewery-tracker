e73478894d6fb1cc992c08b6b00c035f0ede4232
Robbie Greiner <robbieg@gmail.com>
2017-11-08 11:23:28 -0700

    changed to es6 string interpolation

--- src/components/CityCard.js
+++ src/components/CityCard.js
@@ -3,24 +3,14 @@ import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import beer1 from '../assets/beer-tap.svg';
 
-const showFavoriteButton = (brewery, user, favorites, addFavoriteBrewery, getBrewery) => {
+const showFavoriteButton = (brewery, user, favorites, addFavoriteBrewery) => {
   if (user.email) {
     return (
-      <div className='bottom-btns'>
-        <Link to={'/brewery/' + brewery.brewery.id}>
-          <button className='view-beer' onClick={() => getBrewery(brewery.brewery.id)}>DETAILS</button>
-        </Link>
-        <button className='favorite' onClick={() => addFavoriteBrewery(favorites, user.id, 'brewery', brewery.brewery.id, brewery)}>FAVORITE</button>
-      </div>
+      <button className='favorite' onClick={() => addFavoriteBrewery(favorites, user.id, 'brewery', brewery.brewery.id, brewery)}>FAVORITE</button>
     );
   } else {
     return (
-      <div className='bottom-btns'>
-        <Link to={'/brewery/' + brewery.brewery.id}>
-          <button className='view-beer' onClick={() => getBrewery(brewery.brewery.id)}>DETAILS</button>
-        </Link>
-        <Link to='/login'><button className='favorite'>FAVORITE</button></Link>
-      </div>
+      <Link to='/login'><button className='favorite'>FAVORITE</button></Link>
     );
   }
 };
@@ -29,16 +19,21 @@ const CityCard = ({ brewery, getBrewery, addFavoriteBrewery, user, favorites }) 
   if (brewery){
     return (
       <div className={ brewery.brewery.isFav ? 'beer-card brewery-card favorite-card' : 'beer-card brewery-card' }>
-        <Link to={'/brewery/' + brewery.brewery.id}>
+        <Link to={`/brewery/${brewery.brewery.id}`}>
           <h2 onClick={() => getBrewery(brewery.brewery.id)}>{brewery.brewery.name}</h2>
         </Link>
         <h3>{brewery ? brewery.locality + ", " + brewery.region : ''}</h3>
-        <Link to={'/brewery/' + brewery.brewery.id}>
+        <Link to={`/brewery/${brewery.brewery.id}`}>
           {brewery.brewery.images ? <img onClick={() => getBrewery(brewery.brewery.id)} className='brewery-logo' src={brewery.brewery.images.squareMedium}></img>: <img onClick={() => getBrewery(brewery.brewery.id)} className='brewery-icon' src={beer1}></img>}
         </Link>
         <a className='website' href={brewery.website}>{brewery.website}</a>
         <h3 className='type'>{brewery ? brewery.locationTypeDisplay : ''}</h3>
-        {showFavoriteButton(brewery, user, favorites, addFavoriteBrewery, getBrewery)}
+        <div className='bottom-btns'>
+          <Link to={`/brewery/${brewery.brewery.id}`}>
+            <button className='view-beer' onClick={() => getBrewery(brewery.brewery.id)}>DETAILS</button>
+          </Link>
+          {showFavoriteButton(brewery, user, favorites, addFavoriteBrewery)}
+        </div>
       </div>
     );
   } else {
